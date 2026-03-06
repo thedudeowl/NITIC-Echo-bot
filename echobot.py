b9f8e1d2776bf2ee6541523ee0922d9eb29b5f89
thedudeowl <asualum2@gmail.com>
2026-03-06 12:19:46 -0700

    Update echobot.py

--- echobot.py
+++ echobot.py
@@ -1,12 +1,5 @@
 import streamlit as st
 
-st.title("Echo Bot")
-
-# Initialize chat history
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-
-
+prompt = st.chat_input("Say something")
+if prompt:
+    st.write(f"User has sent the following prompt: {prompt}")
