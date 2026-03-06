71812d691504482a4f7cd84fc5c06e87c83dfa86
thedudeowl <asualum2@gmail.com>
2026-03-06 12:18:45 -0700

    Update echobot.py

--- echobot.py
+++ echobot.py
@@ -1,12 +1,5 @@
 import streamlit as st
 
-prompt = st.chat_input("Say something")
-if prompt:
-    st.write(f"User has sent the following prompt: {prompt}")
-
-
-import streamlit as st
-
 st.title("Echo Bot")
 
 # Initialize chat history
