935ca375e7c77039300f84c63875f269c489d3a1
thedudeowl <asualum2@gmail.com>
2026-03-06 11:50:42 -0700

    Update echobot.py

--- echobot.py
+++ echobot.py
@@ -3,3 +3,9 @@ import streamlit as st
 with st.chat_message("user"):
     st.write("Hello 👋")
 
+import streamlit as st
+import numpy as np
+
+with st.chat_message("assistant"):
+    st.write("Hello human")
+    st.bar_chart(np.random.randn(30, 3))
